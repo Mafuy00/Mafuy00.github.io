f0b926c8af3a26fad1b6f6601a647de902d9993e
Jin Yun Lin <yunlin.jin.2022@scis.smu.edu.sg>
2024-09-30 00:59:45 +0800

    q4 mini LT

--- IS216 Past Years/IS216 Past Years (Shared)/2324T1/2324T1-Mini LT/resource/Q4/q4.js
+++ IS216 Past Years/IS216 Past Years (Shared)/2324T1/2324T1-Mini LT/resource/Q4/q4.js
@@ -9,140 +9,74 @@ var url2 = "sales2.json"
 function mergeSales(elem, title, url1, url2) {
     var data = []
     // ADD CODE HERE
-    let url1Data = {}
-    let url2Data = {}
-    let combinedData = {}
+    let mergedSales = {};
 
-    axios.get(url1)
-    .then(response => {
-        let arr1 = response.data;
-        let authorSales = {} 
-        for(let item of arr1.books) {  
-            if(item.author in  authorSales) {
-                url1Data[item.author] += item.sales 
- 
-                // authorSales[item.author] += item.sales 
+    // need to call both url1 and url2 at the same time
+    Promise.all([axios.get(url1), axios.get(url2)])
+    .then((res) => {
+        const [url1Res, url2Res] = res;
+        const url1Data = url1Res.data.books;
+        const url2Data = url2Res.data.books;
+        // combining the 2 dictionaries
+        const combinedArr = url1Data.concat(url2Data);
+        // if the author of combined is in mergedSales, add the values together, 
+        // else insert the item as per normal 
+        for (let item of combinedArr) {
+            if (item.author in mergedSales) {
+                mergedSales[item.author] += item.sales;
             } else {
-                url1Data[item.author] = item.sales
+                mergedSales[item.author] = item.sales;
             }
         }
-    })
+        console.log("mergedSales",mergedSales)
 
-    axios.get(url2)
-    .then(response => {
-        let arr2 = response.data;
-        let authorSales = {} 
-        for(let item of arr2.books) {  
-            if(item.author in  authorSales) { 
-                url2Data[item.author] += item.sales 
-            } else {
-                url2Data[item.author] = item.sales
-            }
+        for(let author in mergedSales) {
+            let x = { "author": author, "sales": mergedSales[author] }
+            data.push(x)
         }
+        display(elem, data, title);
     })
 
+    console.log(mergedSales);
 
-
-    // Loop through the keys of the first dictionary
-    // for (const key in url1Data) {
-    //     // If the key exists in the second dictionary, add the values
-    //     if (url2Data.hasOwnProperty(key)) {
-    //         combinedData[key] = url1Data[key] + url2Data[key];
-    //     } else {
-    //         // Otherwise, just take the value from the first dictionary
-    //         combinedData[key] = url1Data[key];
-    //     }
-        
-
-    // }
-
-    // for (const key in url2Data) {
-    //     // If the key is not already in the result, add it
-    //     if (!combinedData.hasOwnProperty(key)) {
-    //         combinedData[key] = url2Data[key];
-    //     }
-    // }
-
-    for(let author in url1Data) {
-        let x = { "author": author, "sales": url1Data[author] }
-        data.push(x)
-    }
-
-
-    console.log(combinedData);
-    console.log(url1Data);
-    console.log(url2Data);
-
-    display(elem, data, title)
-     
 }
-
-// correct answer by chatgpt
-// function mergeSales(elem, title, url1, url2) {
-//     let url1Data = {};
-//     let url2Data = {};
-//     let combinedData = {};
-
-//     // Fetch both URLs concurrently
-//     Promise.all([
-//         axios.get(url1).then(response => {
-//             let arr1 = response.data;
-//             for (let item of arr1.books) {
-//                 if (item.author in url1Data) {
-//                     url1Data[item.author] += item.sales;
-//                 } else {
-//                     url1Data[item.author] = item.sales;
-//                 }
-//             }
-//         }),
-//         axios.get(url2).then(response => {
-//             let arr2 = response.data;
-//             for (let item of arr2.books) {
-//                 if (item.author in url2Data) {
-//                     url2Data[item.author] += item.sales;
-//                 } else {
-//                     url2Data[item.author] = item.sales;
-//                 }
-//             }
-//         })
-//     ])
-//     .then(() => {
-//         // Merge the two data sets
-//         for (const key in url1Data) {
-//             // If the key exists in the second dictionary, add the values
-//             if (url2Data.hasOwnProperty(key)) {
-//                 combinedData[key] = url1Data[key] + url2Data[key];
-//             } else {
-//                 // Otherwise, just take the value from the first dictionary
-//                 combinedData[key] = url1Data[key];
-//             }
-//         }
-
-//         // Add any additional keys from url2Data
-//         for (const key in url2Data) {
-//             if (!combinedData.hasOwnProperty(key)) {
-//                 combinedData[key] = url2Data[key];
-//             }
-//         }
-
-//         // Log the combined data
-//         console.log(combinedData);
-//         console.log(url1Data);
-//         console.log(url2Data);
-
-//         // Call the display function
-//         display(elem, combinedData, title);
-//     })
-//     .catch(error => {
-//         console.error("Error fetching data:", error);
-//     });
-// }
 
 
 function sortSales(elem, title, url1, url2) {
     var data = []
     // ADD CODE HERE
-    
-    display(elem, data, title)
+    let mergedSales = {};
+
+    Promise.all([axios.get(url1), axios.get(url2)])
+    .then((res) => {
+        const [url1Res, url2Res] = res;
+        const url1Data = url1Res.data.books;
+        const url2Data = url2Res.data.books;
+        const combinedArr = url1Data.concat(url2Data);
+        console.log(combinedArr);
+        for (let item of combinedArr) {
+            if (item.author in mergedSales) {
+                mergedSales[item.author] += item.sales;
+            } else {
+                mergedSales[item.author] = item.sales;
+            }
+        }
+        console.log("mergedSales",mergedSales)
+
+        for(let author in mergedSales) {
+            let x = { "author": author, "sales": mergedSales[author] }
+            data.push(x)
+        }
+
+        data = data
+            .filter((author) => author.sales >= 5)
+            .sort((author1, author2) => {
+                return author2.sales - author1.sales;
+        });
+
+        display(elem, data, title)
+    })
+
+  
      
 }
