d0fd0c6fd3e1545b443c32fdb83e7a42e2845ef6
Jin Yun Lin <yunlin.jin.2022@scis.smu.edu.sg>
2024-10-03 00:33:33 +0800

    2223 mini lab test

--- IS216 Past Years/IS216 Past Years (Shared)/2223T1/2223T1-Mini LT/2223T1-Mini LT/resource/Q3/q3.js
+++ IS216 Past Years/IS216 Past Years (Shared)/2223T1/2223T1-Mini LT/2223T1-Mini LT/resource/Q3/q3.js
@@ -20,10 +20,62 @@ function getSampleJokes() {
   
   // To be completed...
   
+
+  for(let i=0;i<sampleJokes.length;i++){
+    if (sampleJokes[i].type == "twopart"){
+      var tr = document.createElement("tr");
+      var td_sn = document.createElement("td");
+      td_sn.innerText = `${serial}`;
+      var td_cat = document.createElement("td");
+      td_cat.innerText += `${sampleJokes[i].category}`;
+      var td_setup = document.createElement("td");
+      td_setup.innerText += `${sampleJokes[i].setup}`;
+      var td_del = document.createElement("td");
+      td_del.innerText += `${sampleJokes[i].delivery}`;
+      serial += 1;
+      tr.appendChild(td_sn);
+      tr.appendChild(td_cat);
+      tr.appendChild(td_setup);
+      tr.appendChild(td_del);
+      jokeTableTbody.appendChild(tr);
+      // var category = document.createTextNode(sampleJokes[i].category);
+      // var setup = document.createTextNode(sampleJokes[i].setup);
+      // var delivery = document.createTextNode(sampleJokes[i].delivery);
+      // var serialNode = document.createTextNode(serial);
+      // td.appendChild(td.innerText);
+      // td.appendChild(category);
+      // td.appendChild(setup);
+      // td.appendChild(delivery);
+    }
+
+  }
+  // for(let i=0;i<sampleJokes.length;i++){
+  //   jokeTableTbody.innerHTML += `<td>${sampleJokes[i].type}</td>`;
+  // }
 }
+// console.log(jokeTable);
+// console.log(jokeTableTbody);
+// console.log(jokeText);
+// console.log(sampleJokes)
 
 function getJoke() {
-  // To be completed...
+  // To be completed...  
+  var categoryValue = document.getElementById("categoriesDD").value;
+
+  // for(let i=0;i<sampleJokes.length;i++){
+    axios.post(`q3_getJoke.php?query=${categoryValue}`,{
+      category: categoryValue
+    })
+    .then(function (response){
+      var p = document.createElement("p");
+      p.innerText = response.data;
+      jokeText.appendChild(p);
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  // }
 
 }
 
