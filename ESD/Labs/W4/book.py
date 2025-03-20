b322f2273d27c6e8dd2f19b4052ec135f8b95083
Mafuy00 <yunlin.jin.2022@scis.smu.edu.sg>
2025-03-20 18:44:40 +0800

    esd lab 10

--- ESD/Labs/W4/book.py
+++ ESD/Labs/W4/book.py
@@ -106,4 +106,4 @@ def create_book(isbn13):
     ), 201
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ ESD/Labs/W9/resources/activity_log.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+import json
+import os
+from os import environ
+import amqp_lib
+
+rabbit_host = environ.get("rabbit_host") or "localhost"
+rabbit_port = environ.get("rabbit_port") or 5672
+exchange_name = environ.get("exchange_name") or "order_topic"
+exchange_type = environ.get("exchange_type") or "topic"
+queue_name = environ.get("queue_name") or "Activity_Log"
+
+
+def callback(channel, method, properties, body):
+    # required signature for the callback; no return
+    try:
+        error = json.loads(body)
+        print(f"JSON: {error}")
+    except Exception as e:
+        print(f"Unable to parse JSON: {e=}")
+        print(f"Message: {body}")
+    print()
+
+
+if __name__ == "__main__":
+    print(f"This is {os.path.basename(__file__)} - amqp consumer...")
+    try:
+        amqp_lib.start_consuming(
+            rabbit_host, rabbit_port, exchange_name, exchange_type, queue_name, callback
+        )
+    except Exception as exception:
+        print(f"  Unable to connect to RabbitMQ.\n     {exception=}\n")
